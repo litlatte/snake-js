f05946991b8f10c7b589b3dac1226d2c4c30ee80
litlatte <hamza.scuola.02@gmail.com>
2020-06-21 16:12:38 +0200

    Made the game smoother
    Made game smoother by adding frames animations between movements.
    In the Game class you can control the number of frames using the setFrames function.
    
    The animation is handled by 3 main functions:
        - drawAnimation(p) handles the drawing of single frames
        - drawTalePart(p)  raws the tale that becomes smaller until it disappears
        - drawHeadPart(p)  draws the head that becomes bigger until it becomes a full block
    drawRect() is still used to draw the body and draw the final frame

--- js/main.js
+++ js/main.js
@@ -117,6 +117,7 @@ class Game{
     key_down = [83]
     key_left = [65]
     constructor( width, height, block_l, margin, ctx, points_label){
+        this.totalParts=10
         this.started=false
         this.points=0
         this.height = height
@@ -134,11 +135,15 @@ class Game{
         this.points_label=points_label
         this.onGame = false
     }
+
+    setFrames(frames){
+        this.totalParts=number
+    }
+
     drawRect(x, y){
         this.ctx.fillRect((x*this.block_l)+this.margin, (y*this.block_l)+this.margin, this.block_l, this.block_l)
     }
     draw(){
-
         this.ctx.fillStyle = "#F44336" //Margin color
         this.ctx.fillRect(0,0,canvas.width,canvas.height)
         this.ctx.fillStyle = "#333" //Background color
@@ -147,6 +152,8 @@ class Game{
         this.drawRect(this.apple.x, this.apple.y)
         var i = this.snake.x.length-1
         this.ctx.fillStyle = "white"//Snake body color
+        this.drawRect(this.snake.x[i], this.snake.y[i])
+        i--
         for(; i>0 ; i--){
             this.drawRect(this.snake.x[i], this.snake.y[i])
         }
@@ -155,25 +162,93 @@ class Game{
         this.drawRect(this.snake.x[i], this.snake.y[i])
 
     }
-    drawRectSlow(x, y){
-        this.ctx.fillRect((x*this.block_l/10)+this.margin, (y*this.block_l/10)+this.margin, this.block_l, this.block_l)
-    }
-    drawSlow(){
 
-        this.ctx.fillStyle = "#F44336" //Margin color
-        this.ctx.fillRect(0,0,canvas.width,canvas.height)
-        this.ctx.fillStyle = "#333" //Background color
-        this.ctx.fillRect(this.margin, this.margin, this.width*this.block_l, this.height*this.block_l)
-        this.ctx.fillStyle = "#FF5722" //Apple color
-        this.drawRect(this.apple.x, this.apple.y)
-        var i = this.snake.x.length-1
-        this.ctx.fillStyle = "white"//Snake body color
-        for(; i>0 ; i--){
-            this.drawRect(this.snake.x[i], this.snake.y[i])
+    drawTalePart(x, y, part, orientation){
+        var pos_x = x*this.block_l
+        var pos_y = y*this.block_l
+        var block_part = this.block_l /this.totalParts
+        switch(orientation){
+            case 0:
+                pos_y+=block_part*part
+                break
+            case 1:
+                pos_x-=block_part*part
+                break
+            case 2:
+                
+                pos_y-=block_part*part
+                break
+            case 3:
+                pos_x+=block_part*part
+                break
         }
         
-        this.ctx.fillStyle = "#FF9800" //Snake head color
-        this.drawRect(this.snake.x[i], this.snake.y[i])
+        this.ctx.fillRect(pos_x+this.margin, pos_y+this.margin, this.block_l, this.block_l)
+    }
+    drawHeadPart(x, y, part, orientation=this.current_direction){
+        var pos_x = x*this.block_l
+        var pos_y = y*this.block_l
+        var block_part = this.block_l /this.totalParts
+        switch(orientation){
+            case 0:
+                pos_y-=block_part*part
+                break
+            case 1:
+                pos_x+=block_part*part
+                break
+            case 2:
+                pos_y+=block_part*part
+                break
+            case 3:
+                pos_x-=block_part*part
+                break
+        }
+        
+        this.ctx.fillRect(pos_x+this.margin, pos_y+this.margin, this.block_l, this.block_l)
+    }
+
+    drawAnimation(p){
+            this.ctx.fillStyle = "#F44336" //Margin color
+            this.ctx.fillRect(0,0,canvas.width,canvas.height)
+            this.ctx.fillStyle = "#333" //Background color
+            this.ctx.fillRect(this.margin, this.margin, this.width*this.block_l, this.height*this.block_l)
+            this.ctx.fillStyle = "#FF5722" //Apple color
+            this.drawRect(this.apple.x, this.apple.y)
+            var i = this.snake.x.length-1
+            this.ctx.fillStyle = "white"//Snake body color
+            
+            if(this.snake.y[i-1]>this.snake.y[i]){
+                //The snake tale is oriented towards up
+                this.drawTalePart(this.snake.x[i], this.snake.y[i], p, 0)
+
+            }else{
+                if(this.snake.x[i-1]<this.snake.x[i]){
+                    //The snake tale is oriented towards right
+                    this.drawTalePart(this.snake.x[i], this.snake.y[i], p, 1)
+    
+                }else{
+                    if(this.snake.y[i-1]<this.snake.y[i]){
+                        //The snake tale is oriented towards down
+                        this.drawTalePart(this.snake.x[i], this.snake.y[i], p, 2)
+        
+                    }else{
+                        if(this.snake.x[i-1]>this.snake.x[i]){
+                            //The snake tale is oriented towards left
+                            this.drawTalePart(this.snake.x[i], this.snake.y[i], p, 3)
+            
+                        }
+                    }
+                }
+            }
+            
+            i--
+            
+            for(; i>0 ; i--){
+                this.drawRect(this.snake.x[i], this.snake.y[i])
+            }
+            this.drawRect(this.snake.x[i], this.snake.y[i])
+            this.ctx.fillStyle = "#FF9800" //Snake head color
+            this.drawHeadPart(this.snake.x[i], this.snake.y[i], p)
 
     }
     reset(){
@@ -306,7 +381,10 @@ class Game{
             (async () => {
                 var a=0
                 while(this.onGame){
-                    await sleep(100)
+                        for(var p=1; p<=this.totalParts; p++){
+                            await sleep(10)
+                            this.drawAnimation(p)
+                        }
                     this.refresh()
                 }
             })();
